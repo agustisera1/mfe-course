42a05511442ede9a01ae409faa77f45dddbbeafb
Agustin <agustin.tisera@zafin.com>
2024-01-02 14:15:27 -0300

    add Header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,15 @@
 import React from "react";
+import Header from "./Header";
+import { BrowserRouter } from "react-router-dom";
 import { MarketingAppInstance } from "./MarketingAppInstance";
 
-export default () => <MarketingAppInstance />;
+export default () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingAppInstance />;
+      </div>
+    </BrowserRouter>
+  );
+};
